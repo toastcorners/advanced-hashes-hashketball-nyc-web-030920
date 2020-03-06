6637c05432c6234f372a940cb825ef1e352cf27c
Melissa Wise <wise.melissa19@gmail.com>
2020-03-06 17:05:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -124,9 +124,20 @@ end
 end
 end
 
-
-
-
+def shoe_size(input)
+  hash = game_hash
+  hash.each do |location, team|
+  team.each do |attribute, data|
+    if attribute == :players
+    data.each do |player|
+      if player[:player_name] == player_lookup
+        return player[:points]
+      end
+  end
+end
+end
+end
+end
 
 
 
