992e9630f3bb7033cb41ec7eb6dffd7a850e270f
Melissa Wise <wise.melissa19@gmail.com>
2020-03-06 17:01:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -50,8 +50,7 @@ def game_hash
                     :assists => 12, 
                     :steals => 7, 
                     :blocks => 5, 
-                    :slam_dunks => 12}]}
-}}
+                    :slam_dunks => 12}]},
 
   
     :home => {
@@ -103,7 +102,7 @@ def game_hash
                             :assists => 2, 
                             :steals => 4, 
                             :blocks => 11, 
-                            :slam_dunks => 1}]}},
+                            :slam_dunks => 1}]}}
 
 end
 
