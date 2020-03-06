bac984f847565174d83c54c6a2309cab813fb70d
Melissa Wise <wise.melissa19@gmail.com>
2020-03-06 16:55:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -35,7 +35,7 @@ def game_hash
                             :steals => 3, 
                             :blocks => 1, 
                             :slam_dunks => 15 },
-                        {"Mason Plumlee"} => {
+                        {:player_name => "Mason Plumlee"} => {
                             :number => 1, 
                             :shoe => 19, 
                             :points => 26, 
@@ -44,7 +44,7 @@ def game_hash
                             :steals => 3, 
                             :blocks => 8, 
                             :slam_dunks => 5 },
-                         {"Jason Terry"} => {
+                         {:player_name =>"Jason Terry",
                             :number => 31, 
                             :shoe => 15, 
                             :points => 19, 
@@ -57,7 +57,7 @@ def game_hash
    :away => {:team_name => "Charlotte Hornets", 
     :colors => ["Turquoise", "Purple"], 
     :players => [{:player_name => 
-                    {"Jeff Adrien"} => {
+                    "Jeff Adrien",
                     :number => 4, 
                     :shoe => 18, 
                     :points => 10, 
@@ -66,7 +66,7 @@ def game_hash
                     :steals => 2, 
                     :blocks => 7, 
                     :slam_dunks => 2}, 
-                {"Bismack Biyombo"} => {
+                {:player_name => "Bismack Biyombo",
                     :number => 0, 
                     :shoe => 16, 
                     :points => 12, 
@@ -75,7 +75,7 @@ def game_hash
                     :steals => 22, 
                     :blocks => 15, 
                     :slam_dunks => 10 },
-                {"DeSagna Diop"} => {
+                {:player_name => "DeSagna Diop",
                     :number => 2, 
                     :shoe => 14, 
                     :points => 24, 
@@ -84,7 +84,7 @@ def game_hash
                     :steals => 4, 
                     :blocks => 5, 
                     :slam_dunks => 5 },
-                {"Ben Gordon"} => {
+                {:player_name =>"Ben Gordon",
                     :number => 8, 
                     :shoe => 15, 
                     :points => 33, 
@@ -93,7 +93,7 @@ def game_hash
                     :steals => 1, 
                     :blocks => 1, 
                     :slam_dunks => 0},
-                {"Kemba Walker"} => {
+                {:player_name => "Kemba Walker",
                     :number => 33, 
                     :shoe => 15, 
                     :points => 6, 
