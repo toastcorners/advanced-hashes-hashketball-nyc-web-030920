6c89621c2c8229670ebfb9209c935320d9c9952b
Melissa Wise <wise.melissa19@gmail.com>
2020-03-06 16:57:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,7 +8,7 @@ def game_hash
             :colors => ["Black, White"],  
             :players => 
                           [{:player_name => 
-                          :"Alan Anderson" => {
+                          "Alan Anderson",
                             :number => 0, 
                             :shoe => 16, 
                             :points => 22, 
@@ -17,7 +17,7 @@ def game_hash
                             :steals => 3, 
                             :blocks => 1, 
                             :slam_dunks => 1}, 
-                         {"Reggie Evans"} => {
+                         {:player_name => "Reggie Evans",
                             :number => 30, 
                             :shoe => 14, 
                             :points => 12, 
@@ -26,7 +26,7 @@ def game_hash
                             :steals => 12, 
                             :blocks => 12, 
                             :slam_dunks => 7 },
-                         {"Brook Lopez"} => { 
+                         {:player_name =>"Brook Lopez",
                             :number => 11, 
                             :shoe => 17, 
                             :points => 17, 
@@ -35,7 +35,7 @@ def game_hash
                             :steals => 3, 
                             :blocks => 1, 
                             :slam_dunks => 15 },
-                        {:player_name => "Mason Plumlee"} => {
+                        {:player_name => "Mason Plumlee",
                             :number => 1, 
                             :shoe => 19, 
                             :points => 26, 
